b3782eebe60d00c072c7bfc79068b28b5d62595f
radwan <eslamradwan145@gmail.com>
2025-02-07 19:44:15 +0200

    finished signin ui

---
+++ src/SigninPage/SigninPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form.tsx'
+import Figure from './Figure.tsx'
+import styles from './signinPage.module.css'
+
+function SigninPage() {
+    return (
+        <div className={styles.container}>
+            <Figure parentStyles={styles}/>   
+            <Form parentStyles={styles} />   
+        </div>
+    );
+}
+
+export default SigninPage;
