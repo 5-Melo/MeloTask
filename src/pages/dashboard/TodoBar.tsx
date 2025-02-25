e4d49d6c3e2c220a9f43b65fe9008946dea504a0
radwan <eslamradwan145@gmail.com>
2025-02-25 12:57:34 +0200

    finished 95% of work in dashboard

--- src/pages/dashboard/TodoBar.tsx
+++ src/pages/dashboard/TodoBar.tsx
@@ -1,22 +1,44 @@
-import React , {useEffect, useRef} from 'react'
+import React , {useState, useRef} from 'react'
 import styles from './todoBar.module.css'
 import TaskCard from './TaskCard.tsx';
+import { TbLayoutNavbarCollapse } from "react-icons/tb";
+
 
 const TodoBar = ({ barName, tasks, color,dragOver }) => {
+  const bar = useRef(null);
+  const [taskLen, setTaskLen] = useState(tasks.length)
+
+  function collapse(e)
+  {
+    if (bar.current.style.maxHeight === '9vh')
+    {
+      bar.current.style.maxHeight = '80vh';
+      e.target.style.transform = 'rotateX(0deg)'
+
+    }
+    else
+    {
+      e.target.style.transform = 'rotateX(180deg)'
+      bar.current.style.maxHeight = '9vh';
+
+    }
+  }
 
   return (
-    <div className={styles.container} onDragOver={dragOver}>
-        <div className={styles.upperSection} style={{'borderBottom':`.25rem ${color} solid`}}>
+    <div ref={bar} className={styles.container} onDragOver={dragOver}>
+        <div className={styles.upperSection} style={{'borderBottom':`.25rem ${color} solid`,'--dot-color': `${color}`}}>
             <p className={styles.upperSection__heading}>{barName}</p>
-            <span>{tasks.length}</span>
+            <span>{taskLen}</span>
+        <button onClick={collapse} className={styles.container__collapse__button}><TbLayoutNavbarCollapse /></button>
+
         </div>
           <ul className={styles.taskList}>
             {
-                  tasks.map(task => <TaskCard task={task}/>)
+                  tasks.map(task  => <TaskCard task={task}/>)
             }
         </ul>
     </div>
   )
 }
 
-export default TodoBar
+export default TodoBar  
