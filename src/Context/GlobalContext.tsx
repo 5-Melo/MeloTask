f9a9e7c044b29fe4ffd18cf7f67029d35595d719
adham667 <adhamsaeed667@gmail.com>
2025-03-11 00:50:31 +0200

    integrated api into task create/edit

--- src/Context/GlobalContext.tsx
+++ src/Context/GlobalContext.tsx
@@ -1,18 +1,16 @@
-import React, { createContext , useState} from 'react'
+import React, { createContext, useState } from 'react';
 
-
-
-
-const GlobalContext = createContext()
+const GlobalContext = createContext();
 
 export const GlobalProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [popUp, setPopUp] = useState(false);
+    const [projects, setProjects] = useState([]);
 
     return (
-    <GlobalContext.Provider value={{popUp,setPopUp}}>
-        {children}
-    </GlobalContext.Provider>
-)
-}
+        <GlobalContext.Provider value={{ popUp, setPopUp, projects, setProjects }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+};
 
 export default GlobalContext;
